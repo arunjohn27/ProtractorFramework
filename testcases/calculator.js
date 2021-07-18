18c8cbdb75cbd910a1f7a92106cb7275e4755d43
arunjohn27 <arunjohn27@gmail.com>
2021-07-18 16:01:20 +0530

    first commit

---
+++ testcases/calculator.js
@@ -0,0 +1,45 @@
+let homepage = require('../pages/homepage')
+
+describe('test calculator', function() {
+    it('addition test', function(){
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+        
+        //element(by.model('first')).sendKeys('5');
+        homepage.enterFirstNumber('8')
+
+        //element(by.model('second')).sendKeys('5');
+        homepage.enterSecondNumber('2');
+        
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+
+        //let result = element(by.cssContainingText('.ng-binding','10'));
+
+        //expect(result.getText()).toEqual('10');
+        homepage.verifyResult('10');
+        browser.sleep(3000)
+        
+
+    });
+
+    it('subtract test', function(){
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+        
+        //element(by.model('first')).sendKeys('5');
+        homepage.enterFirstNumber('8')
+
+        //element(by.model('second')).sendKeys('5');
+        homepage.enterSecondNumber('2');
+        
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo1();
+
+        //let result = element(by.cssContainingText('.ng-binding','10'));
+
+        //expect(result.getText()).toEqual('10');
+        homepage.verifyResult('10');
+    });
+
+});
